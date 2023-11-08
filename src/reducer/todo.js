22741a54d5712874c35125a59447cd22f278add8
manuelcuesta2005 <macuba2005@gmail.com>
2023-11-08 16:52:48 -0500

    sixth commit ( reducer completed )

--- src/reducer/todo.js
+++ src/reducer/todo.js
@@ -1,6 +1,6 @@
 export const initialState = {
   user: null,
-  todo: [],
+  todos: [],
   currentTodo: null
 }
 
@@ -8,6 +8,14 @@ export function reducer (state, action) {
   switch (action.type) {
     case 'LOGIN': return { ...state, user: action.payload }
     case 'LOGOUT' : return { ...state, user: null }
+    case 'GET_TODO': return { ...state, todos: action.payload }
+    case 'ADD_TODO': return { ...state, todos: [...state.todo, action.payload] }
+    case 'UPDATE_TODO': {
+      const todoIndex = state.todos.findIndex(todo => todo._id === action.payload._id)
+      state.todos[todoIndex] = action.payload
+      return { ...state, currentTodo: null }
+    }
+    case 'DELETE_TODO': return { ...state, todos: state.todo.filter(todo => todo._id !== action.payload) }
     default: return state
   }
 }
