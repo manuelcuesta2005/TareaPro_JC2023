96c41a112ac3796ad921991e2f8b24685c4e9675
manuelcuesta2005 <macuba2005@gmail.com>
2023-11-14 20:59:38 -0500

    fourtenth commit ( formatDate created and implemented in todo card )

---
+++ src/hooks/formatDate.js
@@ -0,0 +1,14 @@
+export function formatDate (date) {
+  const fecha = new Date(date)
+  let month = '' + fecha.getMonth()
+  let day = '' + fecha.getDate()
+  const year = fecha.getFullYear()
+
+  if (month.length < 2) {
+    month = '0' + month
+  }
+  if (day.length < 2) {
+    day = '0' + day
+  }
+  return [day, month, year].join('/')
+}
--- src/view/components/todo/todo.jsx
+++ src/view/components/todo/todo.jsx
@@ -2,27 +2,30 @@
 import { useContext } from 'react'
 import { deleteTodo } from '../../../services/todos'
 import { TodosContext } from '../../../context/todos'
+import { formatDate } from '../../../hooks/formatDate'
 import Edit from '../../../assets/icons/edit.png'
 import Trash from '../../../assets/icons/trash.png'
 import Check from '../../../assets/icons/check.png'
 import './todo.css'
 
 export const Todo = ({ todo }) => {
-  const { state, dispatch } = useContext(TodosContext)
+  const { dispatch } = useContext(TodosContext)
 
   const handleDelete = () => {
-    deleteTodo(state.todos._id)
-      .then(todos => {
-        window.alert('la Tarea: ' + todos.name + '; Ha sido eliminada')
-        dispatch({ type: 'DELETE_TODO', payload: todos._id })
-      })
+    deleteTodo(todo._id)
+      .then(todo => {
+        window.alert('la Tarea: ' + todo.name + '; Ha sido eliminada')
+        dispatch({ type: 'DELETE_TODO', payload: todo._id })
+      }).catch(error =>
+        console.error(error)
+      )
   }
 
   return (
     <div className='card'>
       <h3 className='card__title'>{todo.name}</h3>
       <p className='card__description'>{todo.description}</p>
-      <p className='card__time'>{todo.finishDate}</p>
+      <p className='card__time'>Fecha: {formatDate(todo.finishDate)}</p>
       <div className='card__buttons'>
         <button onClick={handleDelete}><img src={Trash} alt='trash' width={32} /></button>
         <button><img src={Edit} alt='pencil' width={32} /></button>
